67caa2837adc6cc3b7899bebff5586a4d251088f
Erik Jönsson <efjonsson@gmail.com>
2016-09-07 11:54:28 -0400

    make work button bounce

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
-console.log('connected');
+const workButton = document.querySelector('#main_button');
+
+const bouncingButton = setInterval(() => {
+  workButton.style.bottom = '1.5em';
+  setTimeout(() => {
+    workButton.style.bottom = '2em';
+  }, 150);
+}, 3000);
