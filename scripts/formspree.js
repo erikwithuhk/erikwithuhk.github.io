4455332b3ead2b6e54adb3d19086777f0deccb0a
Erik Jönsson <efjonsson@gmail.com>
2016-10-16 20:11:34 -0400

    style alert and remove after 5 seconds

--- scripts/formspree.js
+++ scripts/formspree.js
@@ -1,3 +1,10 @@
+function removeAlert() {
+  const alertNode = document.querySelector('.alert');
+  setTimeout(() => {
+    alertNode.remove();
+  }, 5000);
+}
+
 const $contactForm = $('.contact-form');
 $contactForm.submit(function(e) {
 	e.preventDefault();
@@ -12,10 +19,12 @@ $contactForm.submit(function(e) {
 		success: function(data) {
 			$contactForm.find('.alert--loading').hide();
 			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+      removeAlert();
 		},
 		error: function(err) {
 			$contactForm.find('.alert--loading').hide();
 			$contactForm.append('<div class="alert alert--error">Oops, there was an error.</div>');
+      removeAlert();
 		}
 	});
 });
