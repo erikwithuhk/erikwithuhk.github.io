8a515a5f265e8e4c11834186d40c02295eac003c
Erik Jönsson <efjonsson@gmail.com>
2016-10-16 20:08:42 -0400

    import formspree js

--- scripts/index.js
+++ scripts/index.js
@@ -12,25 +12,3 @@ const bouncingButton = setInterval(() => {
     workButton.style.bottom = '2em';
   }, 450);
 }, 3000);
-
-// function removeValidation(node) {
-//   setTimeout(() => {
-//     node.remove();
-//   }, 5000);
-// }
-//
-// function submitForm(e) {
-//   const contactForm = document.querySelector('.contact-form');
-//   const contactContainerNode = document.querySelector('.contact_section').querySelector('.container');
-//   contactForm.reset();
-//   const validationEl = document.createElement('p');
-//   validationEl.classList.add('contact-validation');
-//   validationEl.textContent = 'Your message has been sent.';
-//   contactContainerNode.appendChild(validationEl);
-//   removeValidation(validationEl);
-// }
-//
-// window.onload = function() {
-//   const contactSubmit = document.querySelector('.submit');
-//   contactSubmit.addEventListener('click', submitForm);
-// };
